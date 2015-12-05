be0429d6f245eb6dd8f6bf547cba98e7eba80cee
Michael Geary <mg@mg.to>
2015-12-05 02:23:35 -0800

    Searchy test

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Searchy {
+	class Program {
+
+		static Searcher _searcher;
+
+		static void Main( string[] args ) {
+			_searcher = new Searcher( "BigFileSort.txt" );
+			Test( "00000000000000000000" );
+			Test( "0002EBLIPQ9GHAOKZ08O" );
+			Test( "0003WS273DEZJWUEQZYM" );
+			Test( "HDLLC9C54UE3VKA87IHA" );
+			Test( "HDLLC9C54UE3VKA87IHW" );
+			Test( "HDLLC9C54UE3VKA87IHZ" );
+			Test( "ZZZYD2XFOSWFK6N0S4WW" );
+			Test( "ZZZZK7XBJ16B5PQ8PG0B" );
+			Test( "ZZZZZZZZZZZZZZZZZZZZ" );
+			Console.ReadKey();
+		}
+
+		static void Test( string value ) {
+			var contains = _searcher.Contains(value);
+			Console.WriteLine( value + ": " + contains );
+		}
+
+	}
+}
---
+++ Searcher.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace Searchy {
+	class Searcher {
+
+		private string _lines;
+		private int _length;
+
+		public Searcher( string path ) {
+			_lines = "\n" + File.ReadAllText( path ) + "\n";
+			_length = _lines.Length;
+		}
+
+		public bool Contains( string find ) {
+			find = "\n" + find + "\n";
+			var low = 0;
+			var high = _length;
+			var length = find.Length;
+
+			for(;;) {
+				if( high - low <= 1 ) {
+					return false;
+				}
+				var mid = ( low + high ) / 2;
+				var start = _lines.LastIndexOf( '\n', mid - 1 );
+				var compare = String.Compare( _lines, start, find, 0, length );
+				if( compare < 0 ) {
+					low = mid;
+				} else if( compare > 0 ) {
+					high = mid;
+				} else {
+					return true;
+				}
+			}
+		}
+	}
+}
